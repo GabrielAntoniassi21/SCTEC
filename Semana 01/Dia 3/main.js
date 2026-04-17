6d11c4ceca843711b9ca18718fe97659ce0492a1
GabrielAntoniassi21 <gabriel.antoniassi21@gmail.com>
2026-04-16 22:00:15 -0300

    alterações primeira aula

---
+++ Semana 01/Dia 3/main.js
@@ -0,0 +1,33 @@
+//comentário
+/*
+  a
+  b   comentario
+  c
+  d
+ */
+
+let numero1 = 5 // declarar variaveis
+let numero2 = 10
+ 
+let resultado = numero1 + numero2
+
+function iniciarBancoDados(){   //declarar função function + nome + () + {}
+  //inicia o banco de dados    só recebe o valor
+}
+
+function soma (a,b) {   // todo recurso dentro do (), se torna um parametro
+   //recebe alguma coisa e executa
+   console.log(a+b) //soma 2 valorres
+
+}
+
+function subtração(a,b){ 
+    return a - b //recebe o valor executa algo e retorna
+}
+
+
+
+const num1 = 5
+const num2 = 10
+
+console.log(num1+num2) //15
